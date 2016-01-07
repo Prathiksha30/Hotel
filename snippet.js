ad1e50742a70b76bc5f67bcfc54a7a968b47dd53
Prathiksha30 <prathiksha_30@hotmail.com>
2016-01-07 12:00:47 +0530

    Done

--- snippet.js
+++
@@ -1,9 +0,0 @@
-var showLeftPush = $('#showLeftPush'),
-    menuLeft = $('#bootshape-menu-s1'),
-    body = $(document.body);
-
-showLeftPush.on('click', function() {
-    $(this).toggleClass('active');
-    body.toggleClass('bootshape-menu-push-toright');
-    menuLeft.toggleClass('bootshape-menu-open');
-});
