ac527fceec5db5fb067724b14f8d7f0b25e3b59c
Asstha Hisssaria <aasthahissaria20@gmail.com>
2016-02-12 21:44:43 +0530

    stafflogin

--- login_success.php
+++ login_success.php
@@ -1,11 +1,14 @@
 <?php 
 	session_start();
-	if(!isset($_SESSION['email_id']))
+	if(!isset($_SESSION['email_id']) || !isset($_SESSION['staffEmail_id']))
 	{
 		header("Location:login.php"); 
 	}
 	else
 	{
+		?>
+		<script> alert('wrong username or password')</script>
+		<?php
 		header("Location:index.php"); 
 	}
 ?>
